eafe7700bbc2c51aba5fe00a33753cb3046b51fe
perhaad@gmail.com <perhaad@gmail.com@99e0e2ae-fd26-cbcf-1f09-8f8f3bce15eb>
2011-12-05 00:41:36 +0000

    check in code for a preliminary value profiler, not yet fully functional

--- trunk/src/libanalysis-devices/analysis-devices.cpp
+++ trunk/src/libanalysis-devices/analysis-devices.cpp
@@ -308,7 +308,7 @@ void analysis_device::inject_analysis(int kernel_to_inject   )
 
 //! No arguments means to test all rules.
 //! Call the vector of analysis_device::analysis_rules to apply the rules
-void analysis_device::test_analysis_rule()
+void analysis_device::test_all_analysis_rule()
 {
 	for(int i = 0; i < analysis_rules.active_rule_count();i++)
 	{
